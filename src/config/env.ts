2936c11dc48d4c39b2a50595fa2d171115885a7a
Zameari <[zameariislam@gmail.com]>
2025-08-15 10:25:59 +0200

    set-up completed

---
+++ src/config/env.ts
@@ -0,0 +1,61 @@
+ import dotenv from 'dotenv'
+
+
+ dotenv.config()
+
+
+ interface EnvConfig{
+    PORT:string,
+    NODE_ENV:'development'|'production',
+    DB_URL:string
+ }
+
+
+ 
+
+
+  const loadEnv=():EnvConfig=>{
+
+
+//      const {PORT,DB_URL,NODE_ENV}=process.env
+
+//     if (!PORT) throw new Error('Missing PORT');
+//   if (!DB_URL) throw new Error('Missing DB_URL');
+
+//   if (!NODE_ENV) throw new Error('Missing NODE_ENV');
+//   if (NODE_ENV !== 'development' && NODE_ENV !== 'production') {
+//     throw new Error('NODE_ENV must be either "development" or "production"');
+//   }
+
+    const requiredEnv:(keyof EnvConfig)[]=['PORT','DB_URL', 'NODE_ENV']
+
+
+          requiredEnv.forEach((key)=>{
+            if(!process.env[key]){
+                throw new Error( `Missing required environment variable ${key} `)
+            }
+          })
+
+
+          return {
+    PORT:process.env.PORT as string,
+    NODE_ENV:process.env.NODE_ENV as 'development'|'production',
+    DB_URL:process.env.DB_URL as string
+
+ }
+    
+
+  }
+
+
+
+
+ 
+
+  
+
+
+
+  export const enVars=loadEnv()
+
+
